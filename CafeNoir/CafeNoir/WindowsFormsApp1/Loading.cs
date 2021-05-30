f4432577233ee0d4ec559eec40b0cdac5c2ef7ec
Poojani Ranasinghe <poojaniranasinghe2000@gmail.com>
2021-05-31 00:25:19 +0530

    Loading and Logging screens

---
+++ CafeNoir/CafeNoir/WindowsFormsApp1/Loading.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeNoir
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+        int startpoint = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            Myprogress.Value = startpoint;
+            if(Myprogress.Value == 100)
+            {
+                Myprogress.Value = 0;
+                timer1.Stop();
+                Form1 log = new Form1();
+                this.Hide();
+                log.Show();
+            }
+
+        }
+
+        private void Loading_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
