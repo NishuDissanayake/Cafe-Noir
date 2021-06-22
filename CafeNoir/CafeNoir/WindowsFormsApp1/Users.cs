a6d591b3b6e3ce720f5414bd0fd2d64d533620e4
amasha26 <amashagunathilaka26@gmail.com>
2021-06-22 08:01:19 +0530

    updated

--- CafeNoir/CafeNoir/WindowsFormsApp1/Users.cs
+++ CafeNoir/CafeNoir/WindowsFormsApp1/Users.cs
@@ -159,5 +159,10 @@ namespace CafeNoir
             ob3.Show();
             this.Hide();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
